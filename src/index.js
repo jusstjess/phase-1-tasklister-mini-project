30749c2d0ef8001c16f289223033c45b2f31587d
Jess Leyva <jessica.leyvat@yahoo.com>
2023-04-21 13:17:44 -0700

    displays tasks - part 1 or project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  addingEventListener()
+  });
+
+
+function addingEventListener() {
+  document.getElementById("create-task-form").addEventListener("submit", handleSumbit )
+ 
+}
+
+function handleSumbit(e){
+  e.preventDefault();
+  const task = document.getElementById("new-task-description").value
+  displayTAsk(task)
+}
+
+function displayTAsk(task){
+
+  const taskUl = document.getElementById('tasks');
+
+  const taskLi = document.createElement('li');
+  
+  taskLi.textContent = task;
+  taskUl.appendChild(taskLi)
+}
