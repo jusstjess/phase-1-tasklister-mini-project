eea0bd89d3b971c22a42d6b0cd64d398a467aa00
Jess Leyva <jessica.leyvat@yahoo.com>
2023-04-21 15:40:40 -0700

    adds priority colors

--- src/index.js
+++ src/index.js
@@ -11,15 +11,44 @@ function addingEventListener() {
 function handleSumbit(e){
   e.preventDefault();
   const task = document.getElementById("new-task-description").value
-  displayTAsk(task)
+  const priorityLevel = parseInt(e.target.priority.value)
+  displayTAsk(task,priorityLevel)
+  
 }
 
-function displayTAsk(task){
+function displayTAsk(task,priorityLevel){
 
   const taskUl = document.getElementById('tasks');
-
   const taskLi = document.createElement('li');
+  const deleteBtn = document.createElement('button')
+
+  deleteBtn.textContent = " x"
+  deleteBtn.addEventListener("click", deletingTasks)
+  
   
   taskLi.textContent = task;
+  taskLi.style.color = getPriorityColor(priorityLevel)
   taskUl.appendChild(taskLi)
+  taskLi.appendChild(deleteBtn)
+
 }
+
+function deletingTasks(e){
+  console.log(e)
+  e.target.parentNode.remove()
+
+}
+function getPriorityColor(priorityLevel){
+  if (priorityLevel === 1){
+    return "red"
+  }
+  else if (priorityLevel === 2){
+    return "orange"
+  }
+  else {
+    return "yellow"
+  }
+}
+
+
+
